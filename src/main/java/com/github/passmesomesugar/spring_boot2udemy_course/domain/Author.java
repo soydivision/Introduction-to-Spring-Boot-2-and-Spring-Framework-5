3f0141ad7b105fcd02756a6efac2c134c0d557fa
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2022-01-26 16:13:08 +0600

    Annotations++; ManyToMany relationship is set

--- src/main/java/com/github/passmesomesugar/spring_boot2udemy_course/domain/Author.java
+++ src/main/java/com/github/passmesomesugar/spring_boot2udemy_course/domain/Author.java
@@ -1,20 +1,44 @@
 package com.github.passmesomesugar.spring_boot2udemy_course.domain;
 
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToMany;
 import java.util.Set;
 
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author() {
     }
 
+
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
