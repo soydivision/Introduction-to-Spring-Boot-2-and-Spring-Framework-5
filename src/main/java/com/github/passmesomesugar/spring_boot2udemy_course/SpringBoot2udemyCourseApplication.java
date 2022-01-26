1d7288ef514df26d3573e41d2c9cd362f7cda00d
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2022-01-26 12:37:33 +0600

    genesis

---
+++ src/main/java/com/github/passmesomesugar/spring_boot2udemy_course/SpringBoot2udemyCourseApplication.java
@@ -0,0 +1,13 @@
+package com.github.passmesomesugar.spring_boot2udemy_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2udemyCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2udemyCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/passmesomesugar/spring_boot2udemy_course/SpringBoot2udemyCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.passmesomesugar.spring_boot2udemy_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2udemyCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
