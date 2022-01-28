e2c3edb75b66b9c1e6c2daaf6d912cb6cb208333
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2022-01-28 16:14:39 +0600

    thymeleaf ++ ~ "/books" book list is displays

--- src/main/java/com/github/passmesomesugar/spring_boot2udemy_course/domain/Author.java
+++ src/main/java/com/github/passmesomesugar/spring_boot2udemy_course/domain/Author.java
@@ -80,7 +80,6 @@ public class Author {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 }
