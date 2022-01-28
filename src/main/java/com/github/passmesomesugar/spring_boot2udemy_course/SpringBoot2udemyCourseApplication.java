85e631c7b72daa7c3667f4cf57f6096866febb27
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2022-01-28 12:07:34 +0600

    H2 enabled

--- src/main/java/com/github/passmesomesugar/spring_boot2udemy_course/SpringBoot2udemyCourseApplication.java
+++ src/main/java/com/github/passmesomesugar/spring_boot2udemy_course/SpringBoot2udemyCourseApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBoot2udemyCourseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot2udemyCourseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2udemyCourseApplication.class, args);
+    }
 }
